adc47f38ef20b79d90d077105431bdd1291e4d29
Jeff MacDonald <jam@zoidtechnologies.com>
2022-12-26 17:34:06 -0500

    - ttyio5/ttyio5.py: FEATURE: indent each line with a string, properly wordwraps.

---
+++ testechoindent.py
@@ -0,0 +1,6 @@
+import ttyio5 as ttyio
+
+buf = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur a tristique ante. Cras posuere augue orci, et porta massa feugiat ultricies. Proin at massa ac risus tempor volutpat. Morbi porttitor eu lectus mattis porta. Vestibulum pellentesque accumsan tellus eu semper. Vivamus accumsan pretium urna, varius elementum mauris sagittis at. Vestibulum eleifend, dolor id ultricies vehicula, quam ligula interdum diam, ut consectetur tortor orci sed nulla. Donec a urna vel magna consequat mattis et quis justo. Vestibulum suscipit tempus venenatis."
+
+ttyio.echo(buf, width=80, indent="*****")
+
