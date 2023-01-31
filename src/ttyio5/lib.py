8d4c9d9844668e404a399271f1840a13067b265a
Jeff MacDonald <jam@zoidtechnologies.com>
2023-01-30 21:03:06 -0500

    - ttyio5/lib.py and Makefile: added

---
+++ src/ttyio5/lib.py
@@ -0,0 +1,19 @@
+def darken(prefix, rgb, percentage):
+  if len(rgb) == 3:
+    (r, g, b) = rgb
+    a = 1
+  elif len(rgb) == 4:
+    (r, g, b, a) = rgb
+  r *= 1-percentage
+  g *= 1-percentage
+  b *= 1-percentage
+  return "%s;2;%d;%d;%d;%dm" % (prefix, r, g, b, a)
+
+def rgb(prefix, rgb):
+  if len(rgb) == 3:
+    (r, g, b) = rgb
+    a = 1
+  elif len(rgb) == 4:
+    (r, g, b, a) = rgb
+  ansi = "%s;2;%s;%s;%s;%sm" % (prefix, r, g, b, a)
+  return ansi
