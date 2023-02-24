4f5230911bf74554394b49891ccb671700670ddd
Jeff MacDonald <jam@zoidtechnologies.com>
2023-02-24 14:34:00 -0500

    - ttyio5/testinputstring.py: basically rewritten. - added display() function which displays the prompt, then the buffer, then does cursor left until cursor is in the right position - backspace no longer requires chr(8)+" "+char(8) sequence thanks to display() - ctrl-u works, but it does not add the deleted text to a ring buffer - arrow keys (left and right) work with value limits (0 to len(buf)) - added a call to bbsengine.initscreen() for debug/mode purposes - insert vs overwrite mode. old rev always appended to end of buf regardless of where the cursor is positioned. - have not tested using the originalvalue param

--- src/testinputstring.py
+++ src/testinputstring.py
@@ -1,90 +1,85 @@
 import bbsengine5 as bbsengine
 import ttyio5 as ttyio
 
-def inputstring(prompt, originalvalue=42, **kw):
-  mask = kw["mask"] if "mask" in kw else None
+def getchinputstring(prompt, originalvalue=42, **kw):
+    mask = kw["mask"] if "mask" in kw else None
 
-  buf = ""
-  pos = 0
+#    buf = str(originalvalue)
+    buf = ""
+    pos = len(buf)
 
-  ttyio.echo(prompt, flush=True, end="")
+    def display():
+      curleft = f"{{cursorleft:{len(buf)-pos}}}"
+      ttyio.echo(f"{{cursorhpos:1}}{{eraseline}}{prompt}{buf}{curleft}", flush=True, end="")
+      bbsengine.setarea(f"pos: {pos} len(buf): {len(buf)} len(prompt): {len(prompt)}")
 
-  mode_insert = True
+    loop = True
+    while loop:
+        if len(buf)-pos < 0:
+          pos = 0
 
-  loop = True
-  while loop:
-    bottombar = f"pos={pos:03d}"
-    if mode_insert is True:
-      bottombar += " INS"
-    else:
-      bottombar += " OVR"
-    bbsengine.setarea(bottombar)
-    ch = ttyio.getch()
-    if ch == "\n":
-        return buf
-    if ch == "KEY_CUTTOBOL": # ^U erase from point to bol, copy to clipboard
-        ttyio.echo("CTRL-U")
-        continue
-    elif ch == "KEY_BACKSPACE":
-        if pos > 0:
-            pos -= 1
-            ttyio.echo(chr(8)+" "+chr(8), flush=True, end="")
-            buf = buf[pos:len(buf)-1]
-        else:
-            ttyio.echo("{bell}", end="",flush=True)
+        display()
+        ch = ttyio.getch()
+        if ch == "KEY_ENTER":
+            return buf
+        elif ch == "KEY_CUTTOBOL": # ^U erase from point to bol, copy to clipboard
+            buf = buf[pos:]
+#            for x in range(0, pos):
+#              ttyio.echo("X", flush=True, end="")
+#              ttyio.echo(chr(8)+" "+chr(8), flush=True, end="")
             pos = 0
-        continue
-    elif ch == "KEY_LEFT":
-        if pos == 0:
-          ttyio.echo("{bell}", end="")
+            continue
+        elif ch == "KEY_BACKSPACE":
+            if pos > 0:
+#                ttyio.echo(chr(8)+" "+chr(8), flush=True, end="")
+                buf = buf[:pos-1]+buf[pos:]
+                pos -= 1
+
+            else:
+                ttyio.echo("{bell}", end="",flush=True)
+                pos = 0
+            continue
+        elif ch == "KEY_LEFT":
+            if pos == 0:
+              ttyio.echo("{bell}", end="", flush=True)
+            else:
+              ttyio.echo("{cursorleft}", end="", flush=True)
+              pos -= 1
+            continue
+        elif ch == "KEY_RIGHT":
+            if pos < len(buf):
+              pos += 1
+              ttyio.echo("{cursorright}", end="", flush=True)
+            else:
+              ttyio.echo("{bell}", end="", flush=True)
+            continue
+        elif ch == "KEY_HOME":
+            ttyio.echo("{cursorleft:%d}" % (pos), end="", flush=True)
+            pos = 0
+            continue
+        elif ch == "KEY_END":
+            if pos < len(buf):
+              z = len(buf) - pos
+              ttyio.echo("{cursorright:%d}" % (z), end="", flush=True)
+              pos = len(buf)
+            continue
+        elif ch[:4] == "KEY_":
+            ttyio.echo("key=%r" % (ch), level="debug")
+            continue
+
+        if mask is not None:
+          ttyio.echo(mask, flush=True, end="")
         else:
-          ttyio.echo("{cursorleft}", end="", flush=True)
-          pos -= 1
-        continue
-    elif ch == "KEY_RIGHT":
-        if pos <= len(buf):
-          pos += 1
-          ttyio.echo("{cursorright}", end="", flush=True)
-        else:
-          ttyio.echo("{bell}", end="", flush=True)
-        continue
-    elif ch == "KEY_HOME":
-      if pos == 0:
-        ttyio.echo("{bell}", end="", flush=True)
-      else:
-        ttyio.echo(f"{{cursorleft:{pos}}}", end="", flush=True)
-        pos = 0
-      continue
-    elif ch == "KEY_END":
-        if pos < len(buf):
-          z = len(buf) - pos
-          ttyio.echo(f"{{cursorright:{z}}}", end="", flush=True)
-          pos = len(buf)
-        continue
-    elif ch == "KEY_INS":
-        mode_insert = True - mode_insert
-        continue
-    elif ch[:4] == "KEY_":
-        ttyio.echo("key=%r" % (ch), level="debug")
-        continue
+          # ttyio5.echo(ch.decode("utf-8"), flush=True, end="")
+          ttyio.echo(ch, flush=True, end="")
 
-    if mask is not None:
-      ttyio.echo(mask, flush=True, end="")
-    else:
-      # ttyio5.echo(ch.decode("utf-8"), flush=True, end="")
-      ttyio.echo(ch, flush=True, end="")
+        buf = buf[:pos] + ch + buf[pos:]
+        pos += 1
 
-    buf = buf[:pos] + ch + buf[pos:] # [pos] = ch
-    pos += 1
-  return buf
+bbsengine.initscreen()
 
-def main(args=None, **kw):
-  originalvalue = 42
-  buf = inputstring("{var:promptcolor}prompt: {var:inputcolor}", originalvalue, multiple=True)
-  ttyio.echo("{/all}")
-  print(buf)
-
-if __name__ == "__main__":
-  bbsengine.initscreen()
-  main()
-  ttyio.echo("{reset}")
+originalvalue = 42
+buf = getchinputstring("{var:promptcolor}prompt: {var:inputcolor}", originalvalue, multiple=True)
+#buf = getchinputstring("prompt: ", originalvalue, multiple=True)
+ttyio.echo("{/all}")
+print(buf)
